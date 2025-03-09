2b08455d8a76044d48213ec285e645948e1f57f5
pashkov256 <pashkovjob@gmail.com>
2025-03-09 16:14:03 +0300

    _

--- golang/medium/347_Top_K_Frequent_Elements/main.go
+++ golang/medium/347_Top_K_Frequent_Elements/main.go
@@ -8,18 +8,14 @@ import (
 func topKFrequent(nums []int, k int) []int {
    if len(nums) == 0 {
 		return []int{}
-  }
+  	}
 
 	frequentMap := make(map[int]int)
-	
 	for _,num := range nums{
 		frequentMap[num]++
 	}
 
-
-
 	pairs := make([][]int, 0, len(frequentMap))
-
 	for key,value := range frequentMap {
 		pairs = append(pairs, []int{key,value})
 	}
@@ -28,13 +24,13 @@ func topKFrequent(nums []int, k int) []int {
 		return pairs[i][1] > pairs[j][1]
 	})
 
-	result := make([]int,0,k)
 
+	
+	result := make([]int,0,k)
 	for i := 0; i < k; i++ {
 		result = append(result, pairs[i][0])
 	}
-	fmt.Printf("%#v",frequentMap)
-	fmt.Printf("%#v",pairs)
+
 	return result
 }
 
