d787d646e36b896ff278c6ec5b173d6de4ba06b9
pashkov256 <pashkovjob@gmail.com>
2025-07-17 18:16:51 +0300

    updt merge two sorted lists

--- golang/easy/21. Merge Two Sorted Lists/main.go
+++ golang/easy/21. Merge Two Sorted Lists/main.go
@@ -8,25 +8,26 @@ type ListNode struct {
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	// printList(list1)
 	dummy := &ListNode{}
 	current := dummy
+
 	for list1 != nil && list2 != nil {
-		if list1.Val < list2.Val {
+		if list1.Val <= list2.Val {
 			current.Next = list1
 			list1 = list1.Next
 		} else {
 			current.Next = list2
 			list2 = list2.Next
 		}
-		
 		current = current.Next
 	}
+
 	if list1 != nil {
 		current.Next = list1
-  } else {
+	} else if list2 != nil {
 		current.Next = list2
-  }
+	}
+
 	return dummy.Next
 }
 
